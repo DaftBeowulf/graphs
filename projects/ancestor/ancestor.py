9b5f9d34b908e1b2ddfa3d101a98b3a856466c10
DaftBeowulf <ajmboosh@gmail.com>
2019-12-10 13:37:07 -0600

    :sparkles: Implements get_parents internal method
    List comprehension for cleaner/faster code -- returns all paired values (parents) when passed a base value (child)

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,14 @@
 
+
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # create function to return parents (neighbors) of children
+    def get_parents(child):
+        return [pair[0] for pair in ancestors if pair[1] == child]
+    # graph will be DIRECTIONAL -- we only want to consider ancestors, so
+    # we will only search upwards through the parental heritage, never searching downward
+    return get_parents(10)
+
+
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
+                  (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+print(earliest_ancestor(test_ancestors, 1))

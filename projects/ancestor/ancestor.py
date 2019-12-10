2cac85ab5a50f25a1d1e1faaaf670af46dd1fc0a
DaftBeowulf <ajmboosh@gmail.com>
2019-12-10 13:40:39 -0600

    :sparkles: Creates Stack class to be used for DFS
    DFS being used since we want to find longest valid path

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,19 @@
+# will be searching for the longest valid path -- calls for DFS using a stack
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.stack)
 
 
 def earliest_ancestor(ancestors, starting_node):

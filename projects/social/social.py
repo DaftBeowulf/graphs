7fa74133afe6a1f92eed35d0d99672adc9c0b851
DaftBeowulf <ajmboosh@gmail.com>
2019-12-11 13:10:52 -0600

    :sparkles: Updates social.py to guided lecture from today
    Completes populate_graph(), converts camel-cased variables to snake_case, adds comments

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,8 +1,10 @@
+import random
 
 
 class User:
     def __init__(self, name):
         self.name = name
+
 
 class SocialGraph:
     def __init__(self):
@@ -30,7 +32,7 @@ class SocialGraph:
         self.users[self.last_id] = User(name)
         self.friendships[self.last_id] = set()
 
-    def populate_graph(self, numUsers, avgFriendships):
+    def populate_graph(self, num_users, avg_friendships):
         """
         Takes a number of users and an average number of friendships
         as arguments
@@ -44,11 +46,34 @@ class SocialGraph:
         self.last_id = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
 
         # Add users
+        for i in range(num_users):
+            self.add_user(f"User {i+1}")
 
         # Create friendships
+        """
+        Hint 1: To create N random friendships, you could create a list with 
+        all possible friendship combinations, shuffle 
+        the list, then grab the first N elements from the list. 
+        You will need to import random to get shuffle.
+        """
+        # total_friendships = avg_friendships * num_users
+
+        # Create a list with all possible friendship combos
+        possible_friendships = []
+        for user_id in self.users:
+            for friend_id in range(user_id+1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+
+        # shuffle the list (uses Fisher-Yates O(N) shuffle)
+        random.shuffle(possible_friendships)
+
+        # grab the first N elements from the list
+        # of times to call add_friendship = avg_friendships * num_users / 2
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
 
     def get_all_social_paths(self, user_id):
         """
